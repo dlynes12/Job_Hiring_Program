cb762ee61cc54e701d20cba13cceac2e7130f511
davidga1 <gabrielle.david@mail.utoronto.ca>
2019-06-27 13:53:13 -0400

    changed ArrayList interviewees from public to private

--- phase1/src/Interviewer.java
+++ phase1/src/Interviewer.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Interviewer implements UserAccess {
 
-    public ArrayList<Applicant> interviewees = new ArrayList();
+    protected ArrayList<Applicant> interviewees = new ArrayList();
 
     void Interview(ArrayList interviewees){
         this.interviewees = interviewees;
