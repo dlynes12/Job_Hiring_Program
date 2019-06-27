bceb36d269cfc9d4d91710f68c6d716766c7cd17
DJ <lynesdan>
2019-06-27 15:54:04 -0400

    Added list of users to the UserAcess system

--- phase1/src/UserAccess.java
+++ phase1/src/UserAccess.java
@@ -1,6 +1,18 @@
-public abstract class UserAccess {
+import java.util.ArrayList;
+
+public class UserAccess {
 
     String userName = null;
     String passWord = null;
+    ArrayList Users = new ArrayList();
+
+
+    UserAccess(ArrayList users){
+        this.Users = users;
+    }
+
+    void addUser(Object user){
+        this.Users.add(user);
+    }
 
 }
