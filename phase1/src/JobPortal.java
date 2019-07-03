1a118106ec57c9005b7ef64e91693c40df271dbf
DJ <lynesdan>
2019-07-03 14:44:50 -0400

    Added functionality for Applicants to create resumes

--- phase1/src/JobPortal.java
+++ phase1/src/JobPortal.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,8 +9,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.File;
 import java.util.Random;
 
 public class JobPortal extends Application {
@@ -36,12 +41,7 @@ public class JobPortal extends Application {
         gridPane.add(button,4,4);
         gridPane.setHgap(40);
 
-        password.setLayoutY(270);
-        password.setLayoutX(220);
-        username.setLayoutY(240);
-        button.setLayoutY(300);
-        username.setLayoutX(220);
-        button.setLayoutX(280);
+
         StackPane box = new StackPane();
         box.getChildren().addAll(gridPane);
         loginScene.getChildren().addAll(box);
@@ -55,10 +55,32 @@ public class JobPortal extends Application {
         button.setOnAction((ActionEvent e) -> { if(user.login(user)) {
             Group JobPortalScene = new Group();
             stage.setScene(new Scene(JobPortalScene, 600, 600));
+            GridPane applicatnResumeUploadButtons = new GridPane();
             Button getResume = new Button("Submit your resume");
             Button getFile = new Button("Open your resume from file");
+            TextField resume = new TextField();
+            getFile.setOnAction((ActionEvent openFile) -> {
+                FileChooser fileChooser = new FileChooser();
+                //fileChooser.setInitialDirectory();
+                fileChooser.setTitle("Open Resume File");
+                fileChooser.setInitialFileName(user.getUsername());
+                File file = fileChooser.showOpenDialog(stage);
+                System.out.println(file);
+                File filename = fileChooser.showSaveDialog(stage);
+
+            });
+            Label labelFileUpload = new Label("Submit Your resume");
+            Label labelEnterResume = new Label("Enter your resume");
+            applicatnResumeUploadButtons.add(getFile,2,2);
+            applicatnResumeUploadButtons.add(getResume,2,4);
+            applicatnResumeUploadButtons.add(labelFileUpload,0,2);
+            applicatnResumeUploadButtons.add(resume,2,3);
+            applicatnResumeUploadButtons.add(labelEnterResume,0,4);
             StackPane resumeUpload = new StackPane();
-            resumeUpload.getChildren().addAll(getFile, getResume);
+            applicatnResumeUploadButtons.setHgap(20);
+            applicatnResumeUploadButtons.setVgap(20);
+            resumeUpload.getChildren().addAll(applicatnResumeUploadButtons);
+
 
             JobPortalScene.getChildren().addAll(resumeUpload);
 
