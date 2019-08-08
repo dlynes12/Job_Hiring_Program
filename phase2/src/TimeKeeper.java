c489207a1ebcead4250f303de84c63906de97e74
DJ <lynesdan>
2019-08-08 09:33:00 -0400

    Added to timekeeper so the jobs will close when expired

--- phase2/src/TimeKeeper.java
+++ phase2/src/TimeKeeper.java
@@ -2,6 +2,7 @@ import javafx.scene.control.DatePicker;
 
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Observable;
 
@@ -9,7 +10,7 @@ public class TimeKeeper extends Observable {
     DatePicker today;
     JobAccess jobManager = new JobAccess();
 
-    public void updateTime(DatePicker datePicker, ArrayList<JobPosting> jobPostings) {
+    public void updateTime(DatePicker datePicker, Collection<ArrayList<JobPosting>> jobPostings) {
         today = datePicker;
         checkCloseDates(jobPostings);
         setChanged(); // marks that the object has changed
@@ -17,11 +18,12 @@ public class TimeKeeper extends Observable {
 
     }
 
-    void checkCloseDates(ArrayList<JobPosting> jobPostings){
-        for (JobPosting job: jobPostings){
+    void checkCloseDates(Collection<ArrayList<JobPosting>> jobPostingsList){
+        for (ArrayList<JobPosting> jobPosting: jobPostingsList){
+            for (JobPosting job : jobPosting){
             if (job.getDateClosed().compareTo(Date.from(today.getValue().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()))<0){
-                jobManager.closeJob(job);
-            }
+                jobManager.closeJob(job, job.getCompany());
+            }}
         }
     }
 }
